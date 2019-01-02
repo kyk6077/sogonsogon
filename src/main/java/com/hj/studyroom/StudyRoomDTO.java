6c16c1c83b01d26ad00d9494acf640a7607d1289
KHJ <kyk6077@naver.com>
2019-01-02 17:13:27 +0900

    make dto,setting pom.xml

--- src/main/java/com/hj/studyroom/StudyRoomDTO.java
+++ src/main/java/com/hj/studyroom/StudyRoomDTO.java
@@ -1,5 +1,47 @@
 package com.hj.studyroom;
 
 public class StudyRoomDTO {
-
+	private int snum;
+	private String lat;
+	private String lng;
+	private String name;
+	private String address;
+	private String description;
+	
+	public int getSnum() {
+		return snum;
+	}
+	public void setSnum(int snum) {
+		this.snum = snum;
+	}
+	public String getLat() {
+		return lat;
+	}
+	public void setLat(String lat) {
+		this.lat = lat;
+	}
+	public String getLng() {
+		return lng;
+	}
+	public void setLng(String lng) {
+		this.lng = lng;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
 }
--- src/main/java/com/hj/visitor/VisitorDTO.java
+++ src/main/java/com/hj/visitor/VisitorDTO.java
@@ -1,5 +1,19 @@
 package com.hj.visitor;
 
 public class VisitorDTO {
-
+	private String id;
+	private String nick_name;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getNick_name() {
+		return nick_name;
+	}
+	public void setNick_name(String nick_name) {
+		this.nick_name = nick_name;
+	}
 }
